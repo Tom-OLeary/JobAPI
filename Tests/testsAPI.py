5f768bb46e49757f49a044047e4b009dfa157a28
Tom OLeary <troleary19@gmail.com>
2020-10-08 20:13:40 -0400

    Update Tests

--- Tests/testsAPI.py
+++ Tests/testsAPI.py
@@ -5,39 +5,33 @@
 import os.path
 import sqlite3
 import getJobsData
+import displayData
+
+URL = getJobsData.START_URL + getJobsData.END_URL
 
 
 def test_get_data():
-    test_location = "http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=51e101b5&app_key" \
-                    "=f9824e1322236e2ede0a4929e3eb27c8&results_per_page=20" \
-                    "&what=javascript%20developer&content-type=application/json "
+    test_location = URL
     test_results = getJobsData.get_data(test_location)
     assert len(test_results) > 0
 
 
 def test_get_bad_data():
-    test_location = "http://api.adzuna.com/v1/apidev/jobs/gb/search/1?app_id=51e101b5&app_key" \
-                    "=f9824e1322236e2ede0a4929e3eb27c8&results_per_page=20" \
-                    "&what=javascript%20developer&content-type=application/json "
+    test_location = getJobsData.START_URL + "xxxyyyyy" + getJobsData.END_URL
     test_results = getJobsData.get_data(test_location)
     assert type(test_results) == list
     assert len(test_results) == 0
 
 
 def test_write_database():
-    test_location = "http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=51e101b5&app_key" \
-                    "=f9824e1322236e2ede0a4929e3eb27c8&results_per_page=20" \
-                    "&what=javascript%20developer&content-type=application/json "
+    test_location = URL
     test_data = getJobsData.get_data(test_location)
     getJobsData.write_database(test_data)
-    os.path.exists('TomOLeary_Sprint2/jobs.db')
-    assert True
+    assert os.path.exists('jobs.db')
 
 
 def test_write_bad_database():
-    test_location = "http://api.adzuna.com/v1/apidev/jobs/gb/search/1?app_id=51e101b5&app_key" \
-                    "=f9824e1322236e2ede0a4929e3eb27c8&results_per_page=20" \
-                    "&what=javascript%20developer&content-type=application/json "
+    test_location = getJobsData.START_URL + "xxxyyyyy" + getJobsData.END_URL
     test_data = getJobsData.get_data(test_location)
     test_results = getJobsData.write_database(test_data)
     assert test_results is None
@@ -49,22 +43,25 @@ def test_tech_filter():
     cursor = connection.cursor()
     sql_select = f"SELECT title FROM jobs WHERE title LIKE '%{text}%';"
     cursor.execute(sql_select)
-    assert True
+    jobs_data = cursor.fetchone()
+    substring = 'Python'
+
+    assert substring in jobs_data[0]
 
 
-def test_location_filter():
-    location = 'london'
+def test_collect_filtered_data():
     connection = sqlite3.connect("jobs.db")
     cursor = connection.cursor()
-    sql_select = f"SELECT title FROM jobs WHERE location LIKE '%{location}%';"
-    cursor.execute(sql_select)
-    assert True
+    results = displayData.collect_filtered_data(cursor, "title", "jobs", "company", "Capita")
+    assert len(results) > 0
 
 
-def test_company_filter():
-    company = 'BlockDox'
+def test_get_filtered_menu():
     connection = sqlite3.connect("jobs.db")
     cursor = connection.cursor()
-    sql_select = f"SELECT title FROM jobs WHERE company LIKE '%{company}%';"
-    cursor.execute(sql_select)
-    assert True
+    results, category_list = displayData.get_filtered_menu(cursor, "javascript", "title", "jobs", "title",
+                                                           "location", "title")
+    substring = 'JavaScript'
+    substring_two = 'python'
+    assert substring in results[0]
+    assert substring_two not in results
